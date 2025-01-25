c225bf318cefa3ae22b929d3e1a1c91c37750628
Suru Abhisekh <abhisekhsuru@gmail.com>
2025-01-25 23:25:24 +0530

    completed the winston logger system and basic setup (identity-service)

---
+++ identityService/src/models/user.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import argon2 from "argon2";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await argon2.hash(this.password);
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await argon2.verify(this.password, candidatePassword);
+  } catch (error) {
+    throw error;
+  }
+};
+
+userSchema.index({ username: "text" }); // User.find({ $text: { $search: 'john' } }) speeds up search queries for large text fields
+
+export const User = mongoose.models.user || mongoose.model("user", userSchema);
